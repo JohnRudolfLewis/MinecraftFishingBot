3115437e6b09855cbeabd931d4437e1458d8d5b1
John Rudolf Lewis <johnrlewis@gmail.com>
2016-06-30 16:24:16 -0700

    Delete blah.js

--- blah.js
+++
@@ -1,9 +0,0 @@
-var moment = require('moment');
-var start = moment();;
-
-function blah() {
-  //console.log(moment.duration(now.diff(start)).humanize());
-  console.log(start.fromNow());
-}
-
-setTimeout(blah, 1500);
